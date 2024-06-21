3e7e5aafb2f3991e4a4e20791c779fe58a01aead
fatiyasalsabila123 <fatiyasalsabila994@gmail.com>
2024-06-21 11:46:45 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
 // import React, { useEffect, useRef, useState } from "react";
 // import Peer from "simple-peer";
-import {RouterProvider } from 'react-router-dom'
-import router from './router';
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 // import io from "socket.io-client";
 import "./App.css";
+import { RoomProvider } from "./Meet/RoomProvider";
 
 // const socket = io.connect("http://localhost:5001");
 
 function App() {
   return (
     <>
-      <RouterProvider  router={router}></RouterProvider >
+      {/* <RoomProvider> */}
+        <RouterProvider router={router}></RouterProvider>
+      {/* </RoomProvider> */}
     </>
-  )
-  
+  );
 }
 
 export default App;
--- client/src/Meet/RoomProvider.jsx
+++ client/src/Meet/RoomProvider.jsx
@@ -1,4 +1,3 @@
-// RoomProvider.js
 import React, { createContext, useContext, useState } from "react";
 
 const RoomContext = createContext();
